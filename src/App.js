98102f09eaefbfe55d200f6c12ea46a466da06ff
Parada <paradafaii28@gmail.com>
2021-05-13 13:48:43 -0400

    install npm package react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import {BrowserRouter as Router} from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Navbar />
-    </div>
+    </Router>
+    
   );
 }
 
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Nav, NavLink } from './NavbarElements';
+
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
+        <>
+            <Nav>
+                <NavLink to="/">Pizza</NavLink>
+            </Nav>
+        </>
     )
 }
 
