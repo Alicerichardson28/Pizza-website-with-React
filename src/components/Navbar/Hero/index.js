476aa1d7cb444e4f68089e6e1f4e5588ecf96c91
Parada <paradafaii28@gmail.com>
2021-06-24 10:41:29 -0400

    style sidebar and create toggle using with onclick icon

--- src/components/Navbar/Hero/index.js
+++ src/components/Navbar/Hero/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../Navbar';
-import Sidebar from '../Sidebar';
+import Sidebar from '../Sidebar'
 import { 
     HeroContainer, 
     HeroContent, 
@@ -11,10 +11,15 @@ import {
 } from './HeroElements'
 
 const Hero = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    }
     return (
         <HeroContainer>
-            <Navbar />
-            <Sidebar />
+            <Navbar toggle={toggle}/>
+            <Sidebar isOpen={isOpen} toggle={toggle}/>
             <HeroContent>
                 <HeroItems>
                     <HeroH1>Greatest Pizza Ever</HeroH1>
--- src/components/Navbar/Sidebar/index.js
+++ src/components/Navbar/Sidebar/index.js
@@ -1,29 +1,30 @@
 import React from 'react'
-import { 
-    SidebarContainer, 
-    Icon, CloseIcon, 
-    SidebarMenu, 
-    SidebarLink, 
-    SideBtnWrap, 
-    SidebarRoute
-} from './SidebarElements'
+import {
+  SidebarContainer,
+  Icon,
+  CloseIcon,
+  SidebarMenu,
+  SidebarLink,
+  SideBtnWrap,
+  SidebarRoute,
+} from "./SidebarElements";
 
-export const Sidebar = () => {
+const Sidebar = ({ isOpen, toggle }) => {
     return (
-        <div>
-            <SidebarContainer>
-                <Icon>
-                    <CloseIcon/>
-                </Icon>
-                <SidebarMenu>
-                    <SidebarLink to="/">Pizzas</SidebarLink>
-                    <SidebarLink to="/">Dessert</SidebarLink>
-                    <SidebarLink to="/">Full Menu</SidebarLink>
-                </SidebarMenu>
-                <SideBtnWrap>
-                    <SidebarRoute to="/">Order Now</SidebarRoute>
-                </SideBtnWrap>
-            </SidebarContainer>
-        </div>
-    )
+      <SidebarContainer onClick={toggle} isOpen={isOpen}>
+        <Icon onClick={toggle}>
+          <CloseIcon />
+        </Icon>
+        <SidebarMenu>
+          <SidebarLink to="/">Pizzas</SidebarLink>
+          <SidebarLink to="/">Dessert</SidebarLink>
+          <SidebarLink to="/">Full Menu</SidebarLink>
+        </SidebarMenu>
+        <SideBtnWrap>
+          <SidebarRoute to="/">Order Now</SidebarRoute>
+        </SideBtnWrap>
+      </SidebarContainer>
+    );
 }
+
+export default Sidebar;
