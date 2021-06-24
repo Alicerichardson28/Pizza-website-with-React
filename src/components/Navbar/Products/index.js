d0f34cacc251c775e93be42190ddc88e0c32920b
Parada <paradafaii28@gmail.com>
2021-06-24 11:17:25 -0400

    create data file and store data un the array of object

---
+++ src/components/Navbar/Products/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Products = () => {
+    return (
+        <ProductsContainer>
+            <ProductsHeading>heading</ProductsHeading>
+            <ProductWrapper>
+                
+            </ProductWrapper>
+        </ProductsContainer>
+    )
+}
+
+export default Products;
